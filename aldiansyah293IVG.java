2fa57fbd6b7dc9799fb72145ffef7034eef90990
Aldiansyah293 <133629172+kirito-san19@users.noreply.github.com>
2023-06-14 18:54:09 +0700

    commit code aldiansyah 293
    Aldiansyah 293 IVG

---
+++ aldiansyah293IVG.java
@@ -0,0 +1,17 @@
+public class aldiansyah293IVG {  
+    public static void main(String[] args) {
+        //deklarasi variabel
+        String nama;
+        int nim;
+
+        //pengisian variabel
+        nama = "Muhammad Aldiansyah Najib A";
+        nim = 293;
+
+        //menampilkan
+        System.out.println("NAMA\t  : " + nama);
+        System.out.println("NIM\t  : " + nim);
+         
+    }
+
+}
