bc015e6076154b1fb3222c10975ef0b4f1b0c5c4
Radhistya Krisna <103236594+radhisty@users.noreply.github.com>
2023-08-13 23:03:34 +0700

    Add files via upload

---
+++ Radhistya.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Radhistya {
+    public static void main(String[] args) {
+        String nama = "Radhistya Krisna Ramadhan";
+        String nim = "2113010288";
+        
+        System.out.println("Nama : "+ nama);
+        System.out.println("NIM : "+nim);
+    }
+}
