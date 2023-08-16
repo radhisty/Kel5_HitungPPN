22020a2f39491b87610ea8c67480ed6cfe2cad30
Ziee99 <141893692+Ziee99@users.noreply.github.com>
2023-08-16 08:50:27 +0700

    Add files via upload

---
+++ Fauzi_2013010194.java
@@ -0,0 +1,17 @@
+public class Fauzi194IVG {  
+    public static void main(String[] args) {
+        //deklarasi variabel
+        String nama;
+        int nim;
+
+        //pengisian variabel
+        nama = "Fauzi Kurniawan Asshidiqie";
+        nim = 194;
+
+        //menampilkan
+        System.out.println("NAMA\t  : " + nama);
+        System.out.println("NIM\t  : " + nim);
+         
+    }
+
+}
